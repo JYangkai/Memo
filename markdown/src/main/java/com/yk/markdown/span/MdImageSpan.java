4d19a161173200fcd2c8aad1aa550b205620a303
JYang <947405980@qq.com>
2021-10-23 13:57:28 +0800

    fix：优化Markdown加载图片功能

--- markdown/src/main/java/com/yk/markdown/span/MdImageSpan.java
+++ markdown/src/main/java/com/yk/markdown/span/MdImageSpan.java
@@ -1,15 +1,16 @@
 package com.yk.markdown.span;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.text.style.DynamicDrawableSpan;
 
-public class MdImageSpan extends DynamicDrawableSpan {
+import com.yk.markdown.utils.BitmapUtils;
 
+public class MdImageSpan extends DynamicDrawableSpan {
     private final Context context;
 
     private final String path;
@@ -24,7 +25,10 @@ public class MdImageSpan extends DynamicDrawableSpan {
         if (TextUtils.isEmpty(path)) {
             return null;
         }
-        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        Activity activity = (Activity) context;
+        int w = activity.getResources().getDisplayMetrics().widthPixels;
+        int h = activity.getResources().getDisplayMetrics().heightPixels;
+        Bitmap bitmap = BitmapUtils.getTheBestBitmap(path, w, h);
         Drawable drawable = new BitmapDrawable(context.getResources(), bitmap);
         drawable.setBounds(0, 0,
                 drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
