ced9b5581b931f9aee9264033562009a02433348
JYang <947405980@qq.com>
2021-10-31 16:39:04 +0800

    fix：优化Markdown图片渲染

--- markdown2/src/main/java/com/yk/markdown2/core/render/word/MdImageRender.java
+++ markdown2/src/main/java/com/yk/markdown2/core/render/word/MdImageRender.java
@@ -12,7 +12,7 @@ public class MdImageRender implements IMdWordRender {
     @Override
     public SpannableStringBuilder render(Context context, MdWord word, BaseMdStyle style) {
         String src = word.getSrc();
-        String path = src.substring(src.indexOf("(") + 1, src.lastIndexOf(")"));
+        String path = src.substring(src.indexOf("]") + 2, src.lastIndexOf(")"));
 
         SpannableStringBuilder spanStr = new SpannableStringBuilder(src);
         spanStr.setSpan(new MdImageSpan(context, path), 0, src.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
